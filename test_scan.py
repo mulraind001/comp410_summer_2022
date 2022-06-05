d6c002320b86d2481b608d72202c0a216b678b58
mkc <claesmk@gmail.com>
2022-06-04 20:44:45 -0400

    Added a sample pdf file and parsing test

--- test_scan.py
+++ test_scan.py
@@ -1,4 +1,7 @@
 import unittest
+import pdfplumber
+import os
+import re
 from scan import show_aggie_pride
 
 
@@ -7,6 +10,26 @@ class ScanTests(unittest.TestCase):
         slogans = show_aggie_pride()
 
         self.assertIn('Aggie Pride - Worldwide', slogans)
+
+    def test_pdf_parse(self):
+        # get a platform safe path to a test pdf file
+        pdf_file = os.path.join('files', 'november_statement.pdf')
+
+        # open the file
+        with pdfplumber.open(pdf_file) as pdf:
+            # loop through each page in the pdf
+            for p in pdf.pages:
+                # extract the text as a str
+                text = p.extract_text()
+
+                # Check for the name that should be in this PDF
+                self.assertIn('Name: John Smith', text)
+
+                # Check for the account number
+                self.assertIn('Account Number: 3942-29992', text)
+
+                # Regex match on the account number
+                self.assertRegexpMatches(text, r'\d+-\d+')
 
 
 if __name__ == '__main__':
