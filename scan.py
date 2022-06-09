c0524868a2102babd502c2abfa6c8e1def3c179e
KaylinBurgess <59934368+KaylinBurgess@users.noreply.github.com>
2022-06-08 21:40:53 -0400

    Update scan.py

--- scan.py
+++ scan.py
@@ -4,7 +4,11 @@ import re
 
 def show_aggie_pride():
 
-    slogan_list = ['Aggie Pride - Worldwide', 'Aggies Do!', 'AGGIE PRIDE!']
+      slogan_list = ['Aggie Pride - Worldwide',
+                    'Aggie Pride!',
+                    'And That\'s On My 1891',
+                    'Aggies ❤️ engineering',
+                    'Aggies Do!']
 
 
     return slogan_list
