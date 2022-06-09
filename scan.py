16f1efeeafcf0f926f83fea5d2faead00b36b041
jscedeno <59890406+jscedeno@users.noreply.github.com>
2022-06-09 17:53:42 -0400

    added slogans

--- scan.py
+++ scan.py
@@ -3,20 +3,10 @@ import re
 
 
 def show_aggie_pride():
-<<<<<<< HEAD
     slogan_list = ['Aggie Pride - Worldwide',
                    'Aggies Do!',
                    'My Aggie Pricde',
-                   'Aggies ❤️ engineering']
-=======
-
-    slogan_list = ['Aggie Pride - Worldwide',
-                    'Aggie Pride!',
-                    'And That\'s On My 1891',
-                    'Aggies ❤️ engineering',
-                    'Aggies Do!']
-
->>>>>>> 6a497769aa1860cfb4f61557df6ad66d1a8d9d0f
+                   'Aggies engineering']
 
     return slogan_list
 
