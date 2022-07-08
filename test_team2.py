d0f8c734a82143fb00d7b26eda2064294592924e
Alexis C <alexis.cathcart0305@gmail.com>
2022-07-07 21:57:18 -0400

    SSN regex and tests added

--- test_team2.py
+++ test_team2.py
@@ -1,5 +1,6 @@
 import unittest
 from team2_pii import find_us_phone_numbers
+from team2_pii import find_us_ssn
 
 
 class Team2TestCases(unittest.TestCase):
@@ -14,6 +15,22 @@ class Team2TestCases(unittest.TestCase):
         # Test invalid
         phone = 'My number is 336-42-1212'
         self.assertFalse(find_us_phone_numbers(phone))
+    
+    def test_us_ssn(self):
+        # Test valid SSN
+        ssn = '111-11-1111'
+        self.assertTrue(find_us_ssn(ssn))
+
+        ssn = '001-01-0001'
+        self.assertTrue(find_us_ssn(ssn))
+
+        # Test invalid SSN
+        ssn = '000-00-0000'
+        self.assertFalse(find_us_ssn(ssn))
+
+        ssn = 'SSN: 123-456-7890'
+        self.assertFalse(find_us_ssn(ssn))
+
 
 
 if __name__ == '__main__':
