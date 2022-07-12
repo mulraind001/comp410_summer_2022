e5401f9594e8d907cbd187c935d4f99fc284c4fe
Elijah Love <105754314+LijahOnDaProgram@users.noreply.github.com>
2022-07-12 19:28:01 -0400

    Team 3 regex unit test cases pii iss #91

--- team3_pii.py
+++ team3_pii.py
@@ -5,7 +5,28 @@ import re
 # https://developers.google.com/edu/python/regular-expressions
 # https://regex101.com/
 def find_us_phone_numbers(text):
-    match = re.search(r'(\d{3}[-.]\d{3}[-.]\d{4})', text)
+    match = re.search(r'(\(\d{3}\)[-.]\d{3}[-.]\d{4}|\d{3}[-.]\d{3}[-.]\d{4})', text)
+    if match:
+        return True
+    return False
+
+
+def find_us_ss_numbers(text):
+    match = re.search(r'(\d{3}-\d{2}-\d{4})', text)
+    if match:
+        return True
+    return False
+
+
+def find_twitter_handles(text):
+    match = re.search(r'(\@[a-zA-Z\d_.]+)', text)
+    if match:
+        return True
+    return False
+
+
+def find_email_addresses(text):
+    match = re.search(r'([a-zA-Z\d[0-9]+@[a-zA-Z]+\.[(com|edu|gov)]+)', text)
     if match:
         return True
     return False
