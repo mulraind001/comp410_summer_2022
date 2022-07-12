5cc2f2ea2583bbea5c502f1d84fb79643d1e5acd
justinblaze17 <davisjustin41@gmail.com>
2022-07-12 18:10:26 -0500

    regex for twt and email

--- test_team2.py
+++ test_team2.py
@@ -1,7 +1,8 @@
 import unittest
 from team2_pii import find_us_phone_numbers
 from team2_pii import find_us_ssn
-
+from team2_pii import find_us_email
+from team2_pii import find_us_twitter_handle
 
 class Team2TestCases(unittest.TestCase):
     def test_us_phone(self):
@@ -31,7 +32,28 @@ class Team2TestCases(unittest.TestCase):
         ssn = 'SSN: 123-456-7890'
         self.assertFalse(find_us_ssn(ssn))
 
+    def test_us_email(self):
+        # Test emails
+        email = 'tochah@freeallapp.com'
+        self.assertTrue(find_us_email(email))
 
+        email = '5464642@nakiuha.com'
+        self.assertTrue(find_us_email(email))
+        # invalid test
+        email = 'jgfj4!3 @ gmail.com'
+        self.assertFalse(find_us_email(email))
+
+    def test_us_twitter(self):
+        # Test Twitter Handles
+        twt = "@junine12"
+        self.assertTrue(find_us_twitter_handle(twt))
+        # invalid test
+
+        twt = "wenter"
+        self.assertFalse(find_us_twitter_handle(twt))
+
+        twt = "@17ds17__"
+        self.assertTrue(find_us_twitter_handle(twt))
 
 if __name__ == '__main__':
     unittest.main()
