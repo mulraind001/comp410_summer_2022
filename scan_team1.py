0a5548ad14ab50dba264f9b6ee5d8d9bb8bf2ff8
Denise Buckhana <57542774+mulraind001@users.noreply.github.com>
2022-07-26 18:33:50 -0400

    Created the scan for team 1

---
+++ scan_team1.py
@@ -0,0 +1,53 @@
+# PII: files/november_statement.pdf: Name: John Smith
+# NO_PII: files/november_statement.pdf: MyBank.com - Banking Statement
+from scan import get_file_text, scan_files
+from team1_pii import find_us_phone_numbers, find_us_street_address, find_twitter_handle, find_credit_card_number, \
+    find_bank_acc_number, find_email_address
+from team2_pii import find_us_ssn
+
+
+def find_pii(text):
+    detected_pii_list = []
+
+    if find_us_phone_numbers(text):
+        detected_pii_list.append('US_Phone_Number')
+
+    if find_email_address(text):
+        detected_pii_list.append('Email_Address')
+
+    if find_bank_acc_number(text):
+        detected_pii_list.append('Banking_Account_Number')
+
+    if find_credit_card_number(text):
+        detected_pii_list.append('Credit_Card_Number')
+
+    if find_us_street_address(text):
+        detected_pii_list.append('US_Street_Address')
+
+    if find_twitter_handle(text):
+        detected_pii_list.append('Twitter_Handle')
+
+    if find_us_ssn(text):
+        detected_pii_list.append('US_SSN')
+
+    return detected_pii_list
+
+
+def main():
+    # get list of files to be scanned
+    files_list = scan_files()
+    for f in files_list:
+        # get the text from the file
+        for text in get_file_text(f):
+            result = find_pii(text)
+            if result:
+                # PII was found
+                print(': '.join(['PII', f, text]))
+                print(result)
+            else:
+                # No PII found in this file
+                print(': '.join(['NO_PII', f, text]))
+
+
+if __name__ == '__main__':
+    main()
